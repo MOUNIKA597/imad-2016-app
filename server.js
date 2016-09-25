914108b0462c7ac0df5595cf2514cc05f65c7b21
MOUNIKA597 <mounika.r597@gmail.com>
2016-09-25 17:36:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/article-one',function (req,res) {
-    res.send('article one requested and displayed here');
-});
-
-app.get('/article-two',function (req,res) {
-    res.send('article two requested and displayed here');
-});
-
-app.get('/article-three',function (req,res) {
-    res.send('article three requested and displayed here');
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Hi! I am your webapp created by mounika
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+        <script>
+alert('i am your java script');
+</script>
+    </body>
+</html>
